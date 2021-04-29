b99270bcba96eb985ea250858dd2af97d17c14b5
Halisia-lab <63605419+Halisia-lab@users.noreply.github.com>
2021-04-29 16:36:01 +0200

    Update email-sender-service-test.ts

---
+++ test/services/email-sender-service-test.ts
@@ -0,0 +1,7 @@
+describe('Email Sender Test', () => {
+    describe('Test mail for 2 remaining items function', () => {
+        it('Should return true when calling', () => {
+            expect(sendMailFor2RemainingItems().toEquals(true);
+        })
+    })
+})
